ceb3f1ecf862efa65b9c5ac93cb5896881529a64
yagoobi@evolbio.mpg.de <81303531+s-yagoobi@users.noreply.github.com>
2024-03-08 13:55:35 +0100

    major changes in genetic_algoritm.py

--- parameters.py
+++ parameters.py
@@ -2,23 +2,25 @@
 # Simulation parameters
 ################################
 # Population size
-population_size = 10
+
+population_size = 4
 
 # Number of edges of a fully-connected graph
-b=int(population_size*(population_size-1)/2)
+b = int(population_size * (population_size - 1) / 2)
 
 # Number of parents producing next generation
 top_parents = 5
 
 # Number of realization until a mutant gets fixed or goes extinct
 number_of_realization = 100
+
 fitness = 2
 
-# The number of parents
-number_of_parents = 10
+# size of the pool
+pool_size = 10
 
 # Number of times the process for finding the optimized graphs repeats
 number_of_GA_iterations = 10
 
-#probability of recombination
-recom_prob=0.1
+# rate of error in recombination
+error_rate = 0.001
